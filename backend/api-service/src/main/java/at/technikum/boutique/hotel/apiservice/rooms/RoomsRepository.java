2a91bdf5c15aaa12af54d0fa7faaf66b6d6ae199
Alexis Schaffer <contact@alexisschaffer.me>
2024-06-02 21:56:59 +0200

    Add and update repositories

--- backend/api-service/src/main/java/at/technikum/boutique/hotel/apiservice/rooms/RoomsRepository.java
+++ backend/api-service/src/main/java/at/technikum/boutique/hotel/apiservice/rooms/RoomsRepository.java
@@ -1,10 +1,23 @@
 package at.technikum.boutique.hotel.apiservice.rooms;
 
 import at.technikum.boutique.hotel.apiservice.rooms.domain.RoomEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface RoomsRepository extends JpaRepository<RoomEntity, Integer>, JpaSpecificationExecutor<RoomEntity> {
+
+    @Query(value = "SELECT r FROM RoomEntity r LEFT OUTER JOIN  BookingEntity b ON r.id = b.room.id " +
+            "AND b.endDate >= :#{#start} " +
+            "AND b.startDate <= :#{#end} " +
+            "WHERE b.room IS NULL"
+    )
+    Page<RoomEntity> findAllAvailableRooms(@Param("start") LocalDate availabilityPeriodStart, @Param("end") LocalDate availabilityPeriodEnd, Pageable pageable);
 }
