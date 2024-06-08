37e70d26e0f6f655088e2cc5cf8dcf083ff7a47f
Tobias Röck <tobias.roeck@outlook.com>
2024-06-08 18:10:33 +0200

    Connected AvailableRoomByPeriod to Backend, implemented view

--- app/boutique-hotel-technikum/src/network/Rooms.ts
+++ app/boutique-hotel-technikum/src/network/Rooms.ts
@@ -6,5 +6,30 @@ export async function getRooms(page: number, pageSize: number = 5): Promise<Room
     Guard.against.less(page, 1);
     Guard.against.less(pageSize, 1)
 
-    return (await axios.get<RoomResponse>(`${import.meta.env.VITE_BACKEND_URL}/api/v1/rooms?page=${page}&pageSize=${pageSize}`)).data;
+    return (await axios.get<RoomResponse>(
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/rooms`,
+        {
+            params: {
+                page,
+                pageSize
+            }
+        }
+    )).data;
+}
+
+export async function getRoomsByPeriod(page: number, start: Date, end: Date, pageSize: number = 5) {
+    Guard.against.less(page, 1);
+    Guard.against.less(pageSize, 1);
+
+    return (await axios.get<RoomResponse>(
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/rooms/available`,
+        {
+            params: {
+                start: start.toISOString().slice(0, 10),
+                end: end.toISOString().slice(0, 10),
+                page,
+                pageSize
+            }
+        }
+    )).data;
 }
