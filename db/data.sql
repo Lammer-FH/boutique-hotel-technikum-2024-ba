1a826e9b4e70ba2818a33bf74510a886d31a134a
ToadsworthLP <fiery.toadsworth@gmail.com>
2024-06-13 16:21:47 +0200

    Unified text language and fixed text encoding issues

--- db/data.sql
+++ db/data.sql
@@ -1,15 +1,15 @@
 USE boutique_hotel
 
 INSERT INTO hotel (name, address, latitude, longitude, directions, contact_phone_number) VALUES
-('Technikum Boutique Hotel', 'Platz 6, 1200 Wien, Austria', 48.2391664, 16.3774409, 'The hotel is located in the heart of Vienna, easily accessible by public transport. The nearest metro station is Dresdnerstrasse on line U6.', '+43 1 1234567');
+('Technikum Boutique Hotel', 'Platz 6, 1200 Wien, Österreich', 48.2391664, 16.3774409, 'Das Hotel befindet sich im Herzen von Wien und ist leicht mit öffentlichen Verkehrsmitteln zu erreichen. Die nächste U-Bahn-Station ist Dresdnerstraße an der Linie U6.', '+43 1 1234567');
 
 INSERT INTO room (hotel_id, room_number, title, description, bed_amount, price) VALUES
-(1, 201, 'Family Deluxe', 'A spacious and modern room with child-friendly amenities and two queen beds.', 2, 150),
-(1, 202, 'Business Single', 'A sleek room with a work desk, ergonomic chair, and a single king bed for the traveling professional.', 1, 130),
-(1, 203, 'Romantic Escape', 'Perfect for couples, this room features a heart-shaped king bed, a jacuzzi, and a stunning city view.', 1, 180),
-(1, 204, 'Executive Suite', 'An elegant suite with a separate living area, king bed, and modern amenities for business and leisure.', 1, 220),
-(1, 205, 'Family Suite', 'A luxurious suite with multiple bedrooms, a play area, and cribs available upon request.', 4, 300),
-(1, 206, 'Presidential Suite', 'The epitome of luxury, offering expansive space, refined decor, and top-notch amenities.', 3, 500);
+(1, 201, 'Familien Deluxe', 'Ein geräumiges und modernes Zimmer mit kinderfreundlichen Annehmlichkeiten und zwei Queen-Betten.', 2, 150),
+(1, 202, 'Business Einzel', 'Ein elegantes Zimmer mit einem Arbeitstisch, ergonomischem Stuhl und einem einzelnen King-Bett für den reisenden Profi.', 1, 130),
+(1, 203, 'Romantikflucht', 'Perfekt für Paare, dieses Zimmer verfügt über ein herzförmiges King-Bett, einen Jacuzzi und eine atemberaubende Aussicht auf die Stadt.', 1, 180),
+(1, 204, 'Executive Suite', 'Eine elegante Suite mit separatem Wohnbereich, King-Bett und modernen Annehmlichkeiten für Geschäftsreisen und Freizeit.', 1, 220),
+(1, 205, 'Familiensuite', 'Eine luxuriöse Suite mit mehreren Schlafzimmern, einem Spielbereich und auf Anfrage verfügbaren Kinderbetten.', 4, 300),
+(1, 206, 'Präsidentensuite', 'Der Inbegriff von Luxus, bietet viel Platz, edles Dekor und erstklassige Annehmlichkeiten.', 3, 500);
 
 INSERT INTO extra (name, description) VALUES
 ('BATHROOM', 'Badezimmer'),
